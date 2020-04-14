4637687ba1ebf44324124c9c6d0e7d8515e4973f
dav3rid <davecodingstuff@gmail.com>
2020-04-14 11:03:32 +0100

    removed solutions in 1-cat-server.js

--- challenges/1-cat-server.js
+++ challenges/1-cat-server.js
@@ -4,50 +4,13 @@ function checkServerStatus(callBack) {}
 
 function fetchBannerContent(callBack) {}
 
-function fetchAllOwners(callBack) {
-  request('/owners', function(error, owners) {
-    const lowerCaseOwners = owners.map(owner => owner.toLowerCase());
-    callBack(error, lowerCaseOwners);
-  });
-}
+function fetchAllOwners(callBack) {}
 
-function fetchCatsByOwner(owner, callBack) {
-  request(`/owners/${owner}/cats`, function(error, cats) {
-    callBack(error, cats);
-  });
-}
+function fetchCatsByOwner(owner, callBack) {}
 
-function fetchCatPics(catNames, callBack) {
-  if (catNames.length === 0) callBack(null);
+function fetchCatPics(catNames, callBack) {}
 
-  const catPics = [];
-
-  catNames.forEach(function(catName) {
-    request(`/pics/${catName}`, function(error, catPic) {
-      if (error !== null) catPics.push('placeholder.jpg');
-      else catPics.push(catPic);
-
-      if (catPics.length === catNames.length) callBack(error, catPics);
-    });
-  });
-}
-
-function fetchAllCats(callBack) {
-  const allCats = [];
-  let catResponses = 0;
-
-  fetchAllOwners(function(error, owners) {
-    owners.forEach(owner => {
-      fetchCatsByOwner(owner, function(error, cats) {
-        catResponses++;
-        allCats.push(...cats);
-        if (catResponses === owners.length) {
-          callBack(error, allCats.sort());
-        }
-      });
-    });
-  });
-}
+function fetchAllCats(callBack) {}
 
 function fetchOwnersWithCats(callBack) {}
 
