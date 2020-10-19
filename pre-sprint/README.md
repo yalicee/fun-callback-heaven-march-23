a52be75aa5df56195b945b6da7cb2e5091c5835e
Mitch Lloyd <mitch@northcoders.com>
2020-10-19 09:29:48 +0100

    Finesse some things

---
+++ pre-sprint/README.md
@@ -0,0 +1,8 @@
+## Pre-sprint instructions
+
+Start with the exercises first.
+
+`cd` into the `exercises` directory and work through exercises `0.js` to `5.js`
+Instructions should be inside each file.
+
+Then head to the evaluation and try working through the tasks with your partner.
--- pre-sprint/exercises/0.js
+++ pre-sprint/exercises/0.js
@@ -1,4 +1,4 @@
-const { dir } = require('console');
 const fs = require('fs');
 
 // Use fs.readdir in order to print the all the js files in library to the terminal
+// You can find reasonable documentation on how to use fs.readdir here: https://www.geeksforgeeks.org/node-js-fs-readdir-method/
