6974bca17f4c644161b2c10a01dc449659bd09a4
russ <1921615+thatrussguy@users.noreply.github.com>
2021-10-12 11:14:44 +0100

    replaces deprecated fs.rmdir usage

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function removeProject(projectName, cb) {
-  fs.rmdir(`./${projectName}`, { recursive: true }, err => {
+  fs.rm(`./${projectName}`, { recursive: true }, err => {
     if (err) console.log(err);
     else console.log('project removed...');
     cb();
