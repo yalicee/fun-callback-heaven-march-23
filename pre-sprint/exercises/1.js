613bf74c0de0e3708cd462e0f1b34f8694eb0964
Paul Rogerson <paul.rogerson@northcoders.com>
2021-11-09 11:51:37 +0000

    Update 1.js

--- pre-sprint/exercises/1.js
+++ pre-sprint/exercises/1.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-// Use fs.readFile in order to read the file contents of './library/hello.txt'
+// Use fs.readFile in order to read the file contents of './library/hello.txt' and console.log the contents to the terminal.
 // You can find documentation on fs.readFile here: https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
