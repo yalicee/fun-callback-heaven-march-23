5010939630efd7aecc4796cbae0c2a90d42acff3
Paul Rogerson <paul.rogerson@northcoders.com>
2021-11-09 11:51:07 +0000

    Update 0.js

--- pre-sprint/exercises/0.js
+++ pre-sprint/exercises/0.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-// Use fs.readdir in order to print the all the js files in library to the terminal
+// Use fs.readdir in order to print the names of all of the `.js` files in the "library" directory to the terminal
 // You can find reasonable documentation on how to use fs.readdir here: https://www.geeksforgeeks.org/node-js-fs-readdir-method/
