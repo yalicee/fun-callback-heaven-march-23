71b4cf4bc0fe62d4bbd824ebe433a8ab451d717a
Paul Rogerson <paul.rogerson@northcoders.com>
2021-11-09 11:55:47 +0000

    Update 3.js

--- pre-sprint/exercises/3.js
+++ pre-sprint/exercises/3.js
@@ -3,4 +3,5 @@ const request = require('./request.js');
 // Use request to make a request to 'https://icanhazdadjoke.com/search' in order to fetch a list of jokes
 
 // Print the list of jokes to the terminal using console.log
-// Try using the documentation to see if you can dynamically alter the number of jokes coming back from the request
+// Try using the documentation (https://icanhazdadjoke.com/api) to see if you can dynamically alter the number of jokes coming back from the request. 
+// This uses URL queries. E.g. `https://icanhazdadjoke.com/search?term=bananas` or `https://icanhazdadjoke.com/search?term=bananas&limit=3`
