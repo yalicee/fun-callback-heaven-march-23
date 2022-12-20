0db5eef3811d96dcf517b744884cb6560941e3a6
Liam310 <liam.duncan310@gmail.com>
2022-12-20 11:02:41 +0000

    remove remnants of a bygone era

--- pre-sprint/exercises/0.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-// Use fs.readdir in order to print the names of all of the `.js` files in the "library" directory to the terminal
-// You can find reasonable documentation on how to use fs.readdir here: https://www.geeksforgeeks.org/node-js-fs-readdir-method/
